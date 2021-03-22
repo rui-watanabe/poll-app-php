38e646e97d778d1203a4d13ee569ab80a7bb4b63
rui-watanabe <wataru610806@gmail.com>
2021-03-22 21:59:59 +0900

    config JS and Css Path #1

--- config.php
+++ config.php
@@ -4,3 +4,5 @@
     define('BASE_CONTEXT_PATH', $match['0'].'/');
   }
   define('BASE_IMAGES_PATH', $match['0'].'/images');
+  define('BASE_JS_PATH', $match['0'].'/js');
+  define('BASE_CSS_PATH', $match['0'].'/css');
