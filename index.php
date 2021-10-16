171852ac7d7629deac0c6d49e1e6be6efa5275b2
rui-watanabe <wataru610806@gmail.com>
2021-10-16 15:32:40 +0900

    refactoring login func #11

--- index.php
+++ index.php
@@ -8,10 +8,10 @@
   require_once SOURCE_PATH.'db/datasource.php';
   require_once SOURCE_PATH.'db/user.query.php';
 
+  session_start();
+
   use db\UserQuery;
   $result = UserQuery::fetchById('test');
-
-  echo CURRENT_URI;
 
   $r_path = str_replace(BASE_CONTEXT_PATH, '', CURRENT_URI);
   $method = strtolower($_SERVER['REQUEST_METHOD']);
