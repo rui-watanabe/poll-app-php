ad85e82b4c8239ef00febc122b48ed421a33b669
rui-watanabe <wataru610806@gmail.com>
2021-11-11 17:53:30 +0900

    add exception handling of auth #20

--- php/libs/auth.php
+++ php/libs/auth.php
@@ -6,43 +6,63 @@
 
   class Auth {
     public static function login($id, $pwd) {
-      $is_success = false;
-      $user = UserQuery::fetchById($id);
-  
-      if(!empty($user) && $user->del_flg !== 1) {
-        if(password_verify($pwd, $user->pwd)) {
-          $is_success = true;
-          UserModel::setSession($user);
-        } else {
-          echo 'Unmatch PassWord';
+      try {
+        $is_success = false;
+        $user = UserQuery::fetchById($id);
+    
+        if(!empty($user) && $user->del_flg !== 1) {
+          if(password_verify($pwd, $user->pwd)) {
+            $is_success = true;
+            UserModel::setSession($user);
+          } else {
+            echo 'Unmatch PassWord';
+          }
         }
+        else {
+          echo 'Not Find User';
+        }
+      } catch(Throwable $e) {
+        $is_success = false;
+        Msg::push(Msg::DEBUG, $e -> getMessage());
+        Msg::push(Msg::ERROR, 'login error');
       }
-      else {
-        echo 'Not Find User';
-      }
+      
       return $is_success;
     }
 
     public static function regist($user) {
-      $is_success = false;
-      $exist_user = UserQuery::fetchById($user->id);
+      try {
+        $is_success = false;
+        $exist_user = UserQuery::fetchById($user->id);
+    
+        if(!empty($exit_user)) {
+          echo 'Exit User';
+          return false;
+        }
+        $is_success = UserQuery::insert($user);
   
-      if(!empty($exit_user)) {
-        echo 'Exit User';
-        return false;
+        if($is_success) {
+          UserModel::setSession($user);
+        }
+      } catch(Throwable $e) {
+        $is_success = false;
+        Msg::push(Msg::DEBUG, $e -> getMessage());
+        Msg::push(Msg::ERROR, 'regist error');
       }
-      
-      $is_success = UserQuery::insert($user);
-
-      if($is_success) {
-        UserModel::setSession($user);
-      }
-
+    
       return $is_success;
     }
 
     public static function isLogin() {
-      $user = UserModel::getSession();
+      try {
+        $user = UserModel::getSession();
+      } catch(Throwable $e) {
+        $user = UserModel::clearSession();
+        Msg::push(Msg::DEBUG, $e -> getMessage());
+        Msg::push(Msg::ERROR, 'occurred error please login');
+        return false;
+      }
+
       if(isset($user)) {
         return true;
       } else {
