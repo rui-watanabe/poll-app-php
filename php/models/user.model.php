e9406e2a66fb8f5a897c8d475dd27cd44e9e43a0
rui-watanabe <wataru610806@gmail.com>
2021-11-15 18:11:13 +0900

    define validate_id func #11

--- php/models/user.model.php
+++ php/models/user.model.php
@@ -8,4 +8,24 @@
     public $del_flg;
 
     protected static $SESSION_NAME = '_user';
+
+    public static function validate_id($val) {
+      $res = true;
+
+      if(empty($val)) {
+        Msg::push(Msg::ERROR, 'please input user id');
+        $res = false;
+      } else {
+        if(strlen($val) > 10) {
+          Msg::push(Msg::ERROR, 'please user id less than 10 digits');
+          $res = false;
+        }
+        if(!preg_match("/^[a-zA-Z0-9]+$/")) {
+          Msg::push(Msg::ERROR, 'please user id half alphanumeric');
+          $res = false;
+        }
+      }
+
+      return $res;
+    }
   }
